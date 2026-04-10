945ff8e87178d36a8739f957e56be60fe1ed2bcf
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-04-10 09:25:10 +0100

    Fix python finding

--- tools/cmake_generate.py
+++ tools/cmake_generate.py
@@ -38,7 +38,7 @@ def main() -> None:
         [
             "cmake",
             *platform_args,
-            f"-DPython3_ROOT_DIR={sys.base_prefix}",
+            f"-DPython3_EXECUTABLE={fix_path(sys.executable)}",
             f"-Dpybind11_DIR={fix_path(pybind11.get_cmake_dir())}",
             f"-Damulet_pybind11_extensions_DIR={fix_path(amulet.pybind11_extensions.__path__[0])}",
             f"-Damulet_rocksdb_DIR={fix_path(os.path.join(RootDir, 'src', 'amulet', 'rocksdb'))}",
